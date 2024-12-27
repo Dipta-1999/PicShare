4e5a15fbcdbe394236c375f5795276b4c290226f
Dipta Ranjan GHOSH <diptaranjan123@gmail.com>
2024-12-27 07:18:23 +0530

    few things changed frontend

--- PicShareFrontEnd/src/page/home.tsx
+++ PicShareFrontEnd/src/page/home.tsx
@@ -1,11 +1,17 @@
 import Gallery from "../components/gallery";
 import NavbarLogin from "../components/navbar-login";
 
-const HomePage : any = () => {
-    return(
+const HomePage: any = () => {
+    return (
         <>
-            <NavbarLogin/>
-            <Gallery/>
+            <NavbarLogin />
+            <div className="flex flex-wrap">
+                <div className="flex flex-col items-center justify-center min-h-screen mt-32 w-full p-2">
+                    <Gallery />
+                </div>
+
+            </div>
+
         </>
     )
 }
