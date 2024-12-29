82ad444613baaf60e861ed31454f84705a511dc3
Dipta Ranjan GHOSH <diptaranjan123@gmail.com>
2024-12-29 13:32:11 +0530

    removed commented codes

--- PicShareBackEnd/src/models/User.ts
+++ PicShareBackEnd/src/models/User.ts
@@ -1,15 +1,3 @@
-// import mongoose from "mongoose";
-
-// const userSchema = new mongoose.Schema({
-//     username: { type: String, required: true, unique: true },
-//     favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Picture' }],
-// });
-
-// export default mongoose.model("User", userSchema);
-
-
-
-
 import mongoose, { Schema } from 'mongoose';
 
 const userSchema = new Schema({
